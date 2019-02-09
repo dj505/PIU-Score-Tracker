2cfb265c43831a128c647b572f612af2e25e7ac7
dj505 <dj505@users.noreply.github.com>
2019-02-09 01:05:37 -0700

    /r/restofthefckingwebapp

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template
-from data import Scores
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from configparser import SafeConfigParser
+from functools import wraps
 
 app = Flask(__name__)
 
-Scores = Scores()
+# Config MySQL
+config = SafeConfigParser()
+config.read('settings.ini')
+app.config['MYSQL_HOST'] = config.get('sql', 'MYSQL_HOST')
+app.config['MYSQL_USER'] =  config.get('sql', 'MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = config.get('sql', 'MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = config.get('sql', 'MYSQL_DB')
+app.config['MYSQL_CURSORCLASS'] = config.get('sql', 'MYSQL_CURSORCLASS')
+
+# Init MySQL
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -15,11 +29,140 @@ def about():
 
 @app.route('/scores')
 def scores():
-    return render_template("scores.html", scores=Scores)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('scores.html', articles=articles)
+    else:
+        msg = 'No articles found'
+        return render_template('scores.html', msg=msg)
+    cur.close()
 
 @app.route('/score/<string:id>/')
 def score(id):
-    return render_template('score.html', id=id)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles WHERE id=%s", [id])
+    article = cur.fetchone()
+    return render_template('score.html', article=article)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=6, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match ya dummy')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = form.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create Cursor
+        cur = mysql.connection.cursor()
+
+        # Execute Query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You have been registered successfully! You may now log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+# User login
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        # Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        if result > 0:
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You have been logged in successfully!')
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Invalid login."
+                return render_template('login.html', error=error)
+            cur.close()
+        else:
+            error = "Username not found."
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You must be logged in to view this!', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/logout')
+@is_logged_in
+def logout():
+    session.clear()
+    flash('You are now logged out.', 'success')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    if result > 0:
+        return render_template('dashboard.html', articles=articles)
+    else:
+        msg = 'No articles found'
+        return render_template('dashboard.html', msg=msg)
+    cur.close()
+
+# Submit Article (replace with scores later)
+class ArticleForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=200)])
+    body = TextAreaField('Body', [validators.Length(min=1)])
+
+@app.route('/add_article', methods=["GET", "POST"])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session['username']))
+        mysql.connection.commit()
+        cur.close()
+        flash('Score submitted!', 'success')
+    return render_template('add_article.html', form=form)
 
 if __name__ == '__main__':
+    app.secret_key = config.get('settings', 'secretkey')
     app.run(debug=True)
