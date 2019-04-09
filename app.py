a6cb47b31f02d297bc8406a5a68952fa1624abed
dj505 <dj505@users.noreply.github.com>
2019-04-09 13:34:42 -0600

    Clean up stuff

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import os
 from werkzeug.utils import secure_filename
 import loadsongs
 from json import load, dump, loads, dumps
+from forms import *
 
 # TODO:
 # - Clean up this mess
@@ -29,12 +30,6 @@ application.config['MYSQL_PASSWORD'] = config.get('sql', 'MYSQL_PASSWORD')
 application.config['MYSQL_DB'] = config.get('sql', 'MYSQL_DB')
 application.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
-difficulties = []
-for i in range(1, 29):
-    difficulties.append(i)
-difficulties = list(zip(difficulties, difficulties))
-
-songlist_pairs = loadsongs.load_song_lists()
 mysql = MySQL(application)
 
 def allowed_file(filename):
@@ -158,16 +153,6 @@ def score(id):
     return render_template('score.html', score=score, image=image)
     application.logger.info(image)
 
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match ya dummy')
-    ])
-    confirm = PasswordField('Confirm Password')
-
 @application.route('/register', methods=['GET', 'POST']) # User registration
 def register():
     form = RegisterForm(request.form)
@@ -245,36 +230,6 @@ def dashboard():
         msg = 'No articles found'
         return render_template('dashboard.html', msg=msg, images=images)
     cur.close()
-
-class ArticleForm(Form): # Submit Article (replace with scores later)
-    song = SelectField('Song', coerce=str, choices=songlist_pairs)
-    lettergrade = SelectField('Letter Grade', coerce=str, choices=(('f', 'F'), ('d', 'D'), ('c', 'C'), ('b', 'B'), ('a', 'A'), ('s', 'S'), ('ss', 'SS'), ('sss', 'SSS')))
-    score = IntegerField('Score')
-    stagepass = SelectField('Stage Pass', coerce=str, choices=(('1', 'True'), ('0', 'False')))
-    type = SelectField('Type', coerce=str, choices=(('singles', 'Singles'), ('doubles', 'Doubles')))
-    difficulty = SelectField('Difficulty', coerce=int, choices=difficulties)
-    platform = SelectField('Platform', coerce=str, choices=(('pad', 'Pad'), ('keyboard', 'Keyboard')))
-    ranked = SelectField('Ranked?', coerce=str, choices=(('1', 'Ranked'), ('0', 'Unranked')))
-
-class SearchForm(Form):
-    filters = (
-    ("score", "Score"),
-    ("difficulty", "Difficulty"),
-    ("stagepass", "Stage Pass"),
-    ("stagebreak", "Stage Break"),
-    ("ranked", "Ranked"),
-    ("unranked", "Unranked"),
-    ("pad", "Pad"),
-    ("keyboard", "Keyboard")
-    )
-    song = SelectField('Song', coerce=str, choices=songlist_pairs)
-    filters = SelectField('Filter', coerce=str, choices=filters)
-
-class ArticleEditForm(Form): # Submit Article (replace with scores later)
-    body = TextAreaField('Body', [validators.Length(min=1)])
-
-class ClaimForm(Form):
-    song = SelectField('Song', coerce=str, choices=songlist_pairs)
 
 @application.route('/add_article', methods=["GET", "POST"]) # Route for add article page
 @is_logged_in
